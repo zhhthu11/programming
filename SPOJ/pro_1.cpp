a40ded61c11be4c6e129be95656fe04f4a36d16b
zhhthu11 <zhhthu11@gmail.com>
2015-09-11 15:28:03 -0500

    test

---
+++ SPOJ/pro_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int check_num = 42;
+    int cur_num;
+    while (true)
+    {
+        cin >> cur_num;
+	if (cur_num == check_num)
+	{
+	    break;
+	}
+	else
+	{
+	    cout << cur_num << endl;
+	}
+    }
+
+    return 0;
+}
