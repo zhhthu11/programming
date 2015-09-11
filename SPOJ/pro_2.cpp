213afd04f3837eb6ab5157317aa64857a937c6d2
zhh <409815422@qq.com>
2015-09-11 01:11:14 -0500

    prime numbers

--- SPOJ/pro_2.cpp
+++ SPOJ/pro_2.cpp
@@ -11,8 +11,7 @@ int main()
 {
     //First step, preprepossing
     //Find prime numbers between 1 and max_num
-    for (int i = 0; i < max_num; i++)
-    {
+    for (int i = 0; i < max_num; i++){
         prime[i] = -1;
         check[i] = 1;
     }
@@ -22,18 +21,13 @@ int main()
 
     for (int i = 2; i < max_num; i++)
     {
-        if (check[i] == 0)
-        {
-            continue;
-        }
+        if (check[i] == 0) continue;
 
         prime[prime_num] = i;
         prime_num += 1;
 
         for (int j = 2; j < (max_num - 1) / i + 1; j++)
-        {
-            check[i * j] = 0;        
-        }
+            check[i * j] = 0;
     }
 
     //Second Step, main procedure
@@ -41,54 +35,32 @@ int main()
     int group, m, n, cur_prime, cur_left, cur_right;
     cin >> group;
 
-    for (int i = 0; i < group; i++)
-    {
-        if (i > 0)
-        {
-            cout << endl;
-        }
+    for (int i = 0; i < group; i++){
+        if (i > 0) cout << endl;
 
         cin >> m >> n;
-        
-        if (m == 1)
-        {
-            m = 2;
-        }
+
+        if (m == 1) m = 2;
 
         for (int j = 0; j < n - m + 1; j++)
-        {
             check[j] = 1;
-        }
 
-        for (int j = 0; j < prime_num; j++)
-        {
+        for (int j = 0; j < prime_num; j++){
             cur_prime = prime[j];
             cur_left = (m - 1) / cur_prime + 1;
             cur_right = n / cur_prime;
-            
-            if (cur_left == 1)
-            {
-                cur_left += 1;
-            }
-            
-            if (cur_right == 0)
-            {
-                break;
-            }
+
+            if (cur_left == 1) cur_left += 1;
+            if (cur_right == 0) break;
 
             for (int k = cur_left; k < cur_right + 1; k++)
-            {
-                check[cur_prime * k - m] = 0;                
-            }
+                check[cur_prime * k - m] = 0;
         }
 
-        for (int j = m; j < n + 1; j++)
-        {
+        for (int j = m; j < n + 1; j++){
             if (check[j - m] == 1)
-            {
                 cout << j << endl;
-            }
-        }        
+        }
     }
 
     return 0;
