93f91a22fba1dbc043fa0a6f21dfef71429a018f
zhh <409815422@qq.com>
2015-09-20 21:53:38 -0500

    delete test

--- test.cpp
+++ test.cpp
@@ -1,29 +1,14 @@
-vector<TreeNode*> generateTrees(int n) {
-    vector<TreeNode*> ret;
-    vector<vector<vector<TreeNode*>>> dp(n,vector<vector<TreeNode*>>(n));
-    helper(1,n,ret,dp);
-    return ret;
+#include <iostream>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+void test_map(){
+    unordered_map<float, int> cur_map;
+    cout << cur_map[0] << endl;
 }
 
-void helper(int start, int end, vector<TreeNode*> &ret,vector<vector<vector<TreeNode*>>> &dp) {
-    if (start > end) {
-        ret.push_back(NULL); return;
-    }
-    if (!dp[start-1][end-1].empty())  {
-        ret = dp[start-1][end-1]; return;
-    }
-    for (int i = start; i <= end; ++i) {
-        vector<TreeNode*> left, right;
-        helper(start, i-1,left,dp);
-        helper(i+1,end,right,dp);
-        for(int j = 0; j < left.size(); ++j) {
-            for (int k = 0; k < right.size(); ++k) {
-                TreeNode* node = new TreeNode(i);
-                node->left = left[j];
-                node->right = right[k];
-                ret.push_back(node);
-            }
-        }
-    }
-    dp[start-1][end-1] = ret;
+int main(){
+    test_map();
+    return 0;
 }
