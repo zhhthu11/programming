3e1566f0d5b90f6661b7faa3f9fa6758dcf4e44d
zhh <409815422@qq.com>
2015-12-14 23:34:57 -0600

    network flow

--- SPOJ/COCONUTS.cc
+++ SPOJ/COCONUTS.cc
@@ -1,82 +1,76 @@
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <algorithm>
-#define MAXD 310
-#define MAXM 91000
-#define INF 0x3f3f3f3f
-int N, M, S, T, d[MAXD], q[MAXD];
-int first[MAXD], e, next[MAXM], v[MAXM], flow[MAXM];
+#include <cstdio>
+#include <climits>
+#include <cstring>
+#include <string>
+using namespace std;
 
-void add(int x, int y, int z){
-    v[e] = y, flow[e] = z;
-    next[e] = first[x], first[x] = e++;    
+const int maxn = 310, maxm = 91000;
+int go[maxm], flow[maxm], nextEdge[maxm], edge_num;
+int fir[maxn], level[maxn], q[maxn];
+int n, m, S, T;
+ 
+void addedge(int x, int y, int flowNum){
+	//add edge from x to y
+	go[edge_num] = y;
+	flow[edge_num] = flowNum;
+	nextEdge[edge_num] = fir[x];
+	fir[x] = edge_num++;
 }
-
-void init(){
-    int x, y;
-    S = 0, T = N + 1, e = 0;
-    memset(first, -1, sizeof(first[0]) * (T + 1));
-    for(int i = 1; i <= N; ++i){
-        scanf("%d", &x);
-        if (x)
-            add(S, i, 1), add(i, S, 0);
-        else
-            add(i, T, 1), add(T, i, 0);
-    }
-    for(int i = 0; i < M; ++i){
-        scanf("%d%d", &x, &y);
-        add(x, y, 1), add(y, x, 1);    
-    }
+ 
+int bfs(){
+	int tail = 0;
+	memset(level, -1, sizeof(level[0]) * (T + 1));
+	level[S] = 0, q[tail++] = S;
+	for (int i = 0; i < tail; ++i)
+		for (int j = fir[q[i]]; j != -1; j = nextEdge[j])
+			if (flow[j] && level[go[j]] == -1){
+				level[go[j]] = level[q[i]] + 1;
+				q[tail++] = go[j];
+				if (go[j] == T) return 1;
+			}
+	return 0;
 }
-
-bool bfs(){
-    int tail = 0;
-    memset(d, -1, sizeof(d[0]) * (T + 1));
-    d[S] = 0, q[tail++] = S;
-
-    for (int i = 0; i < tail; ++i){
-        for (int j = first[q[i]]; j != -1; j = next[j]){
-            if (flow[j] && d[v[j]] == -1){
-                d[v[j]] = d[q[i]] + 1, q[tail++] = v[j];
-                if(v[j] == T)
-                    return true;
-            }
-        }
-    }
-    return false;
+ 
+int dfs(int x, int maxflow){
+	if (x == T) return maxflow;
+	for (int i = fir[x]; i != -1; i = nextEdge[i])
+		if (flow[i] && level[go[i]] == level[x] + 1)
+			if (int t = dfs(go[i], std::min(maxflow, flow[i]))){
+				flow[i] -= t, flow[i ^ 1] += t;
+				return t;
+			}
+	return 0;
 }
-
-int dfs(int cur, int maxflow){
-    if (cur == T)
-        return maxflow;
-    for (int i = first[cur]; i != -1; i = next[i]){
-        if (flow[i] && d[v[i]] == d[cur] + 1){
-            if (int t = dfs(v[i], std::min(maxflow, flow[i]))){
-                flow[i] -= t, flow[i ^ 1] += t;
-                return t;
-            }
-        }
-    }
-    return 0;
-}
-
-void solve(){
-    int ans = 0;
-    while (bfs()){
-        while (true){
-        	int t = dfs(S, INF);
-        	if (t == 0) break;
-            ans += t;
-        }
-    }
-    printf("%d\n", ans);
-}
-
+ 
 int main(){
-    while(scanf("%d%d", &N, &M), N || M){
-        init();
-        solve();    
-    }
-    return 0; 
-}
+	while (scanf("%d%d", &n, &m), n || m){
+		//========Preprocessing==========
+		S = 0, T = n + 1, edge_num = 0;
+		memset(fir, -1, sizeof(fir[0]) * (T + 1));
+ 
+		//============Input==============
+		int x, y;
+		for (int i = 1; i <= n; ++i){
+			scanf("%d", &x);
+			if (x)
+				addedge(S, i, 1), addedge(i, S, 0);
+			else
+				addedge(i, T, 1), addedge(T, i, 0);
+		}
+		for (int i = 0; i < m; ++i){
+			scanf("%d%d", &x, &y);
+			addedge(x, y, 1), addedge(y, x, 1);
+		}
+ 
+		//============MaxFlow============
+		int t, ans = 0;
+		while(bfs()){
+			while ((t = dfs(S, INT_MAX)))
+				ans += t;
+		}
+		printf("%d\n", ans);
+	}
+ 
+	return 0;
+} 
